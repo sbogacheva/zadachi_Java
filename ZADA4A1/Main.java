a8af1f505f7a0a4df39d2b7333b4617084f00038
sbogacheva <s.bogacheva@rambler.ru>
2022-11-04 15:12:16 +0300

    new zada4a

--- ZADA4A1/Main.java
+++ ZADA4A1/Main.java
@@ -1,9 +1,10 @@
+package ZADA4A1;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
       System.out.println("Hello, World!");
-      double tc=38.5;
+      double tc=36.6;
       System.out.println("tc="+tc);
       System.out.println("tf="+(tc*1.8+32));
       System.out.println("tk="+(tc+273.15));
